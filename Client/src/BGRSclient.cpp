f6b0d13a5bbae1e8189da76a516371b5da5bcc4a
Omri Gal Shenhav <shenhav.omri@gmail.com>
2020-12-30 18:03:16 +0200

    interrupted

--- Client/src/BGRSclient.cpp
+++ Client/src/BGRSclient.cpp
@@ -4,25 +4,30 @@
 #include <boost/thread.hpp>
 
 
-void inputTask(ConnectionHandler& handler, bool& terminate){
-        while(!terminate){
-            const short bufsize = 1024;
-            char buf[bufsize];
-            std::cin.getline(buf, bufsize);
-            std::string line(buf);
+void inputTask(ConnectionHandler& handler){
+        while(1){
+            try {
+                const short bufsize = 1024;
+                char buf[bufsize];
+                std::cin.getline(buf, bufsize);
+                std::string line(buf);
 
-            int len = line.length();
-            if (!handler.sendLine(line)&&!terminate) {
-                std::cout << "Disconnected. Exiting...\n" << std::endl;
+                int len = line.length();
+                if (!handler.sendLine(line)) {
+//                    std::cout << "Disconnected. Exiting...\n" << std::endl;
+                    break;
+                }
+
+                // connectionHandler.sendLine(line) appends '\n' to the message. Therefor we send len+1 bytes.
+                std::cout << "Sent " << len + 1 << " bytes to server" << std::endl;
+            }
+            catch (boost::thread_interrupted&) {
                 break;
             }
-            // connectionHandler.sendLine(line) appends '\n' to the message. Therefor we send len+1 bytes.
-            if(!terminate)
-                std::cout << "Sent " << len + 1 << " bytes to server" << std::endl;
         }
 }
 
-void outputTask(ConnectionHandler& handler, bool& terminate){
+void outputTask(ConnectionHandler& handler,boost::thread& inputThread){
     while(1){
         // We can use one of three options to read data from the server:
         // 1. Read a fixed number of characters
@@ -32,7 +37,8 @@ void outputTask(ConnectionHandler& handler, bool& terminate){
         // Get back an answer: by using the expected number of bytes (len bytes + newline delimiter)
         // We could also use: connectionHandler.getline(answer) and then get the answer without the newline char at the end
         if (!handler.getLine(answer)) {
-            std::cout << "Disconnected. Exiting...\n" << std::endl;
+            inputThread.interrupt();
+//            std::cout << "Disconnected. Exiting...\n" << std::endl;
             break;
         }
 
@@ -42,32 +48,32 @@ void outputTask(ConnectionHandler& handler, bool& terminate){
         answer.resize(len - 1);
         std::cout << "Reply: " << answer << " " << len << " bytes " << std::endl << std::endl;
         if (answer == "bye") {
+            inputThread.interrupt();
             std::cout << "Exiting...\n" << std::endl;
-            terminate = false;
             break;
         }
     }
 }
 
-/**
-* This code assumes that the server replies the exact text the client sent it (as opposed to the practical session example)
-*/
 int main (int argc, char *argv[]) {
+    // --       Get server's host and port for connection    ---   //
     if (argc < 3) {
         std::cerr << "Usage: " << argv[0] << " host port" << std::endl << std::endl;
         return -1;
     }
     std::string host = argv[1];
     short port = atoi(argv[2]);
-    
+
+    // --       Connection to Server     ---   //
     ConnectionHandler connectionHandler(host, port);
     if (!connectionHandler.connect()) {
         std::cerr << "Cannot connect to " << host << ":" << port << std::endl;
         return 1;
     }
-    bool terminate = false;
-    boost::thread inputThread(&inputTask,boost::ref(connectionHandler),boost::ref(terminate));
-    boost::thread outputThread(&outputTask,boost::ref(connectionHandler),boost::ref(terminate));
+
+    // --       Run input output with multithreading     ---   //
+    boost::thread inputThread(&inputTask,boost::ref(connectionHandler));
+    boost::thread outputThread(&outputTask,boost::ref(connectionHandler),boost::ref(inputThread));
     inputThread.join();
     outputThread.join();
     std::cerr << "Client terminated" << std::endl;
