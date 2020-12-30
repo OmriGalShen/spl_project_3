987ad57e88a0aec60ea498951540e5fec3b89a7c
Omri Gal Shenhav <shenhav.omri@gmail.com>
2020-12-30 17:17:20 +0200

    Fixed multithreading

--- Client/src/BGRSclient.cpp
+++ Client/src/BGRSclient.cpp
@@ -6,7 +6,6 @@
 
 void inputTask(ConnectionHandler& handler){
         while(1){
-
             const short bufsize = 1024;
             char buf[bufsize];
             std::cin.getline(buf, bufsize);
@@ -31,12 +30,12 @@ void outputTask(ConnectionHandler& handler){
         std::string answer;
         // Get back an answer: by using the expected number of bytes (len bytes + newline delimiter)
         // We could also use: connectionHandler.getline(answer) and then get the answer without the newline char at the end
-//        if (!handler.getLine(answer)) {
-//            std::cout << "Disconnected. Exiting...\n" << std::endl;
-//            break;
-//        }
+        if (!handler.getLine(answer)) {
+            std::cout << "Disconnected. Exiting...\n" << std::endl;
+            break;
+        }
 
-		int len=answer.length();
+		auto len=answer.length();
 		// A C string must end with a 0 char delimiter.  When we filled the answer buffer from the socket
 		// we filled up to the \n char - we must make sure now that a 0 char is also present. So we truncate last character.
         answer.resize(len-1);
@@ -47,65 +46,6 @@ void outputTask(ConnectionHandler& handler){
         }
     }
 }
-
-
-
-//class ReadTask{
-//private:
-//    ConnectionHandler& _handler;
-//public:
-//    ReadTask (ConnectionHandler& handler) : _handler(handler){}
-//
-//    void operator()(){
-//        while(1){
-//
-//            const short bufsize = 1024;
-//            char buf[bufsize];
-//            std::cin.getline(buf, bufsize);
-//            std::string line(buf);
-//
-//            int len=line.length();
-//            if (!_handler.sendLine(line)) {
-//                std::cout << "Disconnected. Exiting...\n" << std::endl;
-//                break;
-//            }
-//            // connectionHandler.sendLine(line) appends '\n' to the message. Therefor we send len+1 bytes.
-//            std::cout << "Sent " << len+1 << " bytes to server" << std::endl;
-//        }
-//    }
-//};
-//
-//class ReceiveTask{
-//private:
-//    ConnectionHandler& _handler;
-//public:
-//    ReceiveTask (ConnectionHandler& handler) : _handler(handler) {}
-//    void operator()(){
-//    while(1){
-//        // We can use one of three options to read data from the server:
-//        // 1. Read a fixed number of characters
-//        // 2. Read a line (up to the newline character using the getline() buffered reader
-//        // 3. Read up to the null character
-//        std::string answer;
-//        // Get back an answer: by using the expected number of bytes (len bytes + newline delimiter)
-//        // We could also use: connectionHandler.getline(answer) and then get the answer without the newline char at the end
-//        if (!_handler.getLine(answer)) {
-//            std::cout << "Disconnected. Exiting...\n" << std::endl;
-//            break;
-//        }
-//
-//		int len=answer.length();
-//		// A C string must end with a 0 char delimiter.  When we filled the answer buffer from the socket
-//		// we filled up to the \n char - we must make sure now that a 0 char is also present. So we truncate last character.
-//        answer.resize(len-1);
-//        std::cout << "Reply: " << answer << " " << len << " bytes " << std::endl << std::endl;
-//        if (answer == "bye") {
-//            std::cout << "Exiting...\n" << std::endl;
-//            break;
-//        }
-//    }
-//    }
-//};
 
 /**
 * This code assumes that the server replies the exact text the client sent it (as opposed to the practical session example)
@@ -123,15 +63,6 @@ int main (int argc, char *argv[]) {
         std::cerr << "Cannot connect to " << host << ":" << port << std::endl;
         return 1;
     }
-	
-	//From here we will see the rest of the ehco client implementation:
-//    ReadTask read(connectionHandler);
-//    ReceiveTask receive(connectionHandler);
-//    std::thread th1(std::ref(read));
-//    std::thread th2(std::ref(ReceiveTask));
-//    th1.join();
-//    th2.join();
-
 
     boost::thread inputThread(&inputTask,boost::ref(connectionHandler));
     boost::thread outputThread(&outputTask,boost::ref(connectionHandler));
