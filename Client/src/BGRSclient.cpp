3337359bd7ab446eaf116009627f2d9e79fb337a
Omri Gal Shenhav <shenhav.omri@gmail.com>
2020-12-31 19:28:16 +0200

    Fixed 2 threads instead of 3

--- Client/src/BGRSclient.cpp
+++ Client/src/BGRSclient.cpp
@@ -27,34 +27,6 @@ void inputTask(ConnectionHandler& handler){
         }
 }
 
-void outputTask(ConnectionHandler& handler,boost::thread& inputThread){
-    while(1){
-        // We can use one of three options to read data from the server:
-        // 1. Read a fixed number of characters
-        // 2. Read a line (up to the newline character using the getline() buffered reader
-        // 3. Read up to the null character
-        std::string answer;
-        // Get back an answer: by using the expected number of bytes (len bytes + newline delimiter)
-        // We could also use: connectionHandler.getline(answer) and then get the answer without the newline char at the end
-        if (!handler.getLine(answer)) {
-            inputThread.interrupt();
-//            std::cout << "Disconnected. Exiting...\n" << std::endl;
-            break;
-        }
-
-        auto len = answer.length();
-        // A C string must end with a 0 char delimiter.  When we filled the answer buffer from the socket
-        // we filled up to the \n char - we must make sure now that a 0 char is also present. So we truncate last character.
-        answer.resize(len - 1);
-        std::cout << "Reply: " << answer << " " << len << " bytes " << std::endl << std::endl;
-        if (answer == "bye") {
-            inputThread.interrupt();
-            std::cout << "Exiting...\n" << std::endl;
-            break;
-        }
-    }
-}
-
 int main (int argc, char *argv[]) {
     // --       Get server's host and port for connection    ---   //
     if (argc < 3) {
@@ -73,9 +45,34 @@ int main (int argc, char *argv[]) {
 
     // --       Run input output with multithreading     ---   //
     boost::thread inputThread(&inputTask,boost::ref(connectionHandler));
-    boost::thread outputThread(&outputTask,boost::ref(connectionHandler),boost::ref(inputThread));
+
+    while(1){
+        // We can use one of three options to read data from the server:
+        // 1. Read a fixed number of characters
+        // 2. Read a line (up to the newline character using the getline() buffered reader
+        // 3. Read up to the null character
+        std::string answer;
+        // Get back an answer: by using the expected number of bytes (len bytes + newline delimiter)
+        // We could also use: connectionHandler.getline(answer) and then get the answer without the newline char at the end
+        if (!connectionHandler.getLine(answer)) {
+            inputThread.interrupt();
+//            std::cout << "Disconnected. Exiting...\n" << std::endl;
+            break;
+        }
+
+        auto len = answer.length();
+        // A C string must end with a 0 char delimiter.  When we filled the answer buffer from the socket
+        // we filled up to the \n char - we must make sure now that a 0 char is also present. So we truncate last character.
+        answer.resize(len - 1);
+        std::cout << "Reply: " << answer << " " << len << " bytes " << std::endl << std::endl;
+        if (answer == "bye") {
+            inputThread.interrupt();
+            std::cout << "Exiting...\n" << std::endl;
+            break;
+        }
+    }
+
     inputThread.join();
-    outputThread.join();
     std::cerr << "Client terminated" << std::endl;
     return 0;
 }
