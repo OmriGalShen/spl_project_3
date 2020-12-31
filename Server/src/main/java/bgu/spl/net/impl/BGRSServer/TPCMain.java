b45981ddadca964db4f7eeb781ab85485e25f470
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2020-12-31 13:27:47 +0200

    Fixed main classes

--- Server/src/main/java/bgu/spl/net/impl/BGRSServer/TPCMain.java
+++ Server/src/main/java/bgu/spl/net/impl/BGRSServer/TPCMain.java
@@ -1,7 +1,16 @@
 package bgu.spl.net.impl.BGRSServer;
 
+import bgu.spl.net.srv.Server;
+
 public class TPCMain {
     public static void main(String[] args) {
-
+        if (args.length < 1) {
+            System.out.println("You must supply port");
+        }
+        Server.threadPerClient(
+                Integer.parseInt(args[0]), // port
+                ()->new BGRSMessagingProtocol(),
+                ()->new BGRSMessageEncoderDecoder()
+        ).serve();
     }
 }
