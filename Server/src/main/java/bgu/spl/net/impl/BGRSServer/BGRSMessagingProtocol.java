9263e3e2c12045b4eab13d13aca92c63f8ee27ff
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2021-01-01 10:34:30 +0200

    Started implementing Server protocol

--- Server/src/main/java/bgu/spl/net/impl/BGRSServer/BGRSMessagingProtocol.java
+++ Server/src/main/java/bgu/spl/net/impl/BGRSServer/BGRSMessagingProtocol.java
@@ -7,12 +7,52 @@ import bgu.spl.net.impl.BGRSServer.Messages.RGRSMessage;
 
 public class BGRSMessagingProtocol implements MessagingProtocol<RGRSMessage>{
     private boolean shouldTerminate = false;
+    private boolean isLogin = false;
+    private boolean isAdmin = false;
 
     @Override
     public RGRSMessage process(RGRSMessage msg) {
-        System.out.println("op code:"+msg.getOpCode());
-//        RGRSMessage response = new ErrorMessage((short) 13,(short) 5);
-        RGRSMessage response = new ACKMessage((short) 12,msg.getOpCode(),"a");
+        short opCode = msg.getOpCode();
+        RGRSMessage response = new ACKMessage((short) 12,msg.getOpCode(),"a");;
+        //        RGRSMessage response = new ErrorMessage((short) 13,(short) 5);
+        switch (opCode){
+            case 1: //ADMINREG
+                System.out.println("ADMINREG");
+                break;
+            case 2: //STUDENTREG
+                System.out.println("STUDENTREG");
+                break;
+            case 3: //LOGIN
+                System.out.println("LOGIN");
+                break;
+            case 4: //LOGOUT
+                System.out.println("LOGOUT");
+                break;
+            case 5: //COURSEREG
+                System.out.println("COURSEREG");
+                break;
+            case 6: //KDAMCHECK
+                System.out.println("KDAMCHECK");
+                break;
+            case 7: //COURSESTAT
+                System.out.println("COURSESTAT");
+                break;
+            case 8: //STUDENTSTAT
+                System.out.println("STUDENTSTAT");
+                break;
+            case 9: //ISREGISTERED
+                System.out.println("ISREGISTERED");
+                break;
+            case 10: //UNREGISTER
+                System.out.println("UNREGISTER");
+                break;
+            case 11: //MYCOURSES
+                System.out.println("MYCOURSES");
+                break;
+            default: // opcode not valid
+                response = new ErrorMessage((short) 13,(short) 13);
+                break;
+        }
         return response;
     }
 
