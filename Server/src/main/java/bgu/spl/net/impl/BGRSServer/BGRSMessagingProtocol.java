9374b9ff78e6c00a17a94335dd2b9d5dd48c4783
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2021-01-01 22:14:12 +0200

    Testing protocol

--- Server/src/main/java/bgu/spl/net/impl/BGRSServer/BGRSMessagingProtocol.java
+++ Server/src/main/java/bgu/spl/net/impl/BGRSServer/BGRSMessagingProtocol.java
@@ -1,9 +1,9 @@
 package bgu.spl.net.impl.BGRSServer;
 
 import bgu.spl.net.api.MessagingProtocol;
-import bgu.spl.net.impl.BGRSServer.Messages.ACKMessage;
-import bgu.spl.net.impl.BGRSServer.Messages.ErrorMessage;
-import bgu.spl.net.impl.BGRSServer.Messages.RGRSMessage;
+import bgu.spl.net.impl.BGRSServer.Messages.*;
+
+import java.util.ArrayList;
 
 public class BGRSMessagingProtocol implements MessagingProtocol<RGRSMessage>{
     private boolean shouldTerminate = false;
@@ -13,39 +13,62 @@ public class BGRSMessagingProtocol implements MessagingProtocol<RGRSMessage>{
     @Override
     public RGRSMessage process(RGRSMessage msg) {
         short opCode = msg.getOpCode();
-        System.out.println("In protocol, received opcode: "+opCode);
-        RGRSMessage response = new ACKMessage((short) 12,msg.getOpCode(),"a");;
+        RGRSMessage response = new ACKMessage((short) 12,msg.getOpCode(),"your message was received");;
         //        RGRSMessage response = new ErrorMessage((short) 13,(short) 5);
+        ArrayList<String> operations;
+        short courseNum;
         switch (opCode){
             case 1: //ADMINREG
                 System.out.println("ADMINREG");
+                operations= ((RequestMessage) msg).getOperations();
+                System.out.println("operations");
+                operations.forEach(System.out::println);
                 break;
             case 2: //STUDENTREG
                 System.out.println("STUDENTREG");
+                operations= ((RequestMessage) msg).getOperations();
+                System.out.println("operations");
+                operations.forEach(System.out::println);
                 break;
             case 3: //LOGIN
                 System.out.println("LOGIN");
+                operations= ((RequestMessage) msg).getOperations();
+                System.out.println("operations");
+                operations.forEach(System.out::println);
                 break;
             case 4: //LOGOUT
                 System.out.println("LOGOUT");
                 break;
             case 5: //COURSEREG
                 System.out.println("COURSEREG");
+                courseNum = ((CourseInfoMessage) msg).getCourseNum();
+                System.out.println("courseNum:"+courseNum);
                 break;
             case 6: //KDAMCHECK
                 System.out.println("KDAMCHECK");
+                courseNum = ((CourseInfoMessage) msg).getCourseNum();
+                System.out.println("courseNum:"+courseNum);
                 break;
             case 7: //COURSESTAT
                 System.out.println("COURSESTAT");
+                courseNum = ((CourseInfoMessage) msg).getCourseNum();
+                System.out.println("courseNum:"+courseNum);
                 break;
             case 8: //STUDENTSTAT
                 System.out.println("STUDENTSTAT");
+                operations= ((RequestMessage) msg).getOperations();
+                System.out.println("operations");
+                operations.forEach(System.out::println);
                 break;
             case 9: //ISREGISTERED
                 System.out.println("ISREGISTERED");
+                courseNum = ((CourseInfoMessage) msg).getCourseNum();
+                System.out.println("courseNum:"+courseNum);
                 break;
             case 10: //UNREGISTER
                 System.out.println("UNREGISTER");
+                courseNum = ((CourseInfoMessage) msg).getCourseNum();
+                System.out.println("courseNum:"+courseNum);
                 break;
             case 11: //MYCOURSES
                 System.out.println("MYCOURSES");
